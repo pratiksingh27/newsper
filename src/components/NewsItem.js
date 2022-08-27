dfad46e7bb5b7dbd65495d58e3a0c422f9ef6a2a
pratiksingh27 <pratik27march@gmail.com>
2022-08-27 20:43:45 +0530

    fixing error of deployment

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
         <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{describtion}</p>
-        <a href="/" className="btn btn-primary">Go somewhere</a>
+        <a href="/" className="btn btn-primary">Read more</a>
         </div>
         </div>
       </div>
