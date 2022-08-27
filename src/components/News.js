f74dedbd9afd894888a6a6d0f92a29c437fb438e
pratiksingh27 <pratik27march@gmail.com>
2022-08-27 20:40:34 +0530

    work on news card

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,21 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news components.
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className='container my-3'>
+        <h2>Newsper - Top Headlines</h2>
+        <div className="row">
+        <div className="col-md-4">
+        <NewsItem title="hello" describtion="hii guys"/>
+        </div>
+        <div className="col-md-4">
+        <NewsItem title="hello" describtion="hii guys"/>
+        </div>
+        <div className="col-md-4">
+        <NewsItem title="hello" describtion="hii guys"/>
+        </div>
+        
+        </div>
+        
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 export class NewsItem extends Component {
   render() {
+  let {title, describtion} = this.props;
+
     return (
       <div>
-        This is a newsitems
+        <div className="card" style={{width: "18rem"}}>
+        <img src="..." class="card-img-top" alt="..." />
+        <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{describtion}</p>
+        <a href="/" className="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
       </div>
     )
   }
